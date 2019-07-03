e64cba442354de617ed2136421ab7a0eeb0eea68
Esteban Morell <estebanmorellb@gmail.com>
2019-07-03 05:00:30 -0500

    starting wins

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,87 @@
 $(document).ready(function() {
- 
-    var vwins= document.getElementById("wins");
-    var vwinscounter= 0;
-    var vloses= document.getElementById("loses");
-    var vlosescounter= 0;
-    var vguesrem= document.getElementById("guesrem");
-    var vguesremcounter= 6;
-    var vgueswords= document.getElementById("lgueswords");
-    var vword= document.getElementById("word");
-    
-   
-    var game={
-        wordsarray: ["vampire","zombie","monster","underground"],
-        lose: true,
-        word:"",
-        key:"",
-        choosing_random: function() {
-            return this.wordsarray[Math.floor(Math.random() * this.wordsarray.length)];           
-        },
-        empty_word: function() {
-            vword.textContent="";   /////////////////borrar         
-            for (let i = 0; i < this.word.length; i++) {
-                vword.textContent = vword.textContent + "_ ";                               
-            }
-            console.log(vword.textContent);
-        },
-        reset: function(){
-            vwins.textContent= 0;
-            vloses.textContent= 0;
-            vguesrem.textContent= 6;
-            vgueswords.textContent= "";
-            vword.textContent="";
-            vwinscounter= 0;
-            vlosescounter= 0;
-            vguesremcounter=6;
-        },
-        sust_letter: function(index, replace){
-           return vword.textContent.substring(0, index) + replace + vword.textContent.substring(index + 1);
-        },
+  var vwins = document.getElementById("wins");
+  var vwinscounter = 0;
+  var vloses = document.getElementById("loses");
+  var vlosescounter = 0;
+  var vguesrem = document.getElementById("guesrem");
+  var vguesremcounter = 6;
+  var vgueswords = document.getElementById("gueswords");
+  var vword = document.getElementById("word");
 
+  var game = {
+    wordsarray: ["vampirea", "zombie", "monster", "underground"],
+    lose: true,
+    word: "",
+    userkey: "",
+    choosing_random: function() {
+      return this.wordsarray[
+        Math.floor(Math.random() * this.wordsarray.length)
+      ];
+    },
+    empty_word: function() {
+      vword.textContent = ""; /////////////////borrar
+      for (let i = 0; i < this.word.length; i++) {
+        vword.textContent = vword.textContent + "_ ";
+      }
+      console.log(vword.textContent);
+    },
+    reset: function() {
+      // vwins.textContent= 0;
+      // vloses.textContent= 0;
+      vguesrem.textContent = 6;
+      vgueswords.textContent = "";
+      vword.textContent = "";
+      // vwinscounter= 0;
+      // vlosescounter= 0;
+      vguesremcounter = 6;
+      this.word = this.choosing_random();
+      this.empty_word();
+      this.userkey = "";
+      this.lose = true;
+    },
 
+    sust_letter: function(index) {
+      var temp = vword.textContent;
+      vword.textContent = "";
 
-    };
+      for (var j = 0; j < temp.length; j++) {
+        if (j === index * 2) {
+          vword.textContent = vword.textContent + this.userkey;
+        } else {
+          vword.textContent = vword.textContent + temp[j];
+        }
+      }
+    }
+  };
 
+  game.word = game.choosing_random(); ////////
+  game.empty_word(); ////////
+  console.log(game.word); ////////
+  game.lose = false; ///////////
 
-    game.word = game.choosing_random();////////
-    console.log(game.word);////////
-    game.empty_word();////////
-    key="q";//////////
-    var n = game.word.indexOf("a");
-    console.log(n);
-    
+  document.onkeyup = function(event) {
+    if (!game.lose) {
+      game.userkey = event.key;
+      var ifmatch = false;
+      if (vgueswords.textContent.indexOf(game.userkey) === -1) {
+        vgueswords.textContent = vgueswords.textContent + " " + game.userkey;
 
+        for (var i = 0; i < game.word.length; i++) {
+          if (game.word[i] === game.userkey) {
+            game.sust_letter(i);
+            ifmatch = true;
+          }
+        }
+        if (!ifmatch) {
+          vguesremcounter--;
+          vguesrem.textContent = vguesremcounter;
+          if (vguesremcounter === 0) {
+            game.lose = true;
+            vlosescounter++;
+            vloses.textContent = vlosescounter;
+          }
+        }
+      }
+    }
+  };
 });
